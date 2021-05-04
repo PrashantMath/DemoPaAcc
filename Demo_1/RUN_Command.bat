644f2176eb62211f2ce3a37f018df5068ab811a4
Prashant M <pmathapa@mathworks.com>
2021-05-04 10:48:50 +0530

    Added PS server

--- Demo_1/RUN_Command.bat
+++ Demo_1/RUN_Command.bat
@@ -10,7 +10,7 @@ set build_cmd=build.bat
 
 polyspace-configure.exe -allow-overwrite -allow-build-error -prog %PROG% -author jenkins -output-options-file %PROG%.psopts %build_cmd% || EXIT /B 200
 
-polyspace-bug-finder.exe -options-file "%WSP%\Demo_1\target.opts" -options-file "%WSP%\Demo_1\%PROG%.psopts" -results-dir "%WSP%\R_BF_%BUILD_NUMBER%"   ||  EXIT /B 200
+polyspace-bug-finder-server.exe -options-file "%WSP%\Demo_1\target.opts" -options-file "%WSP%\Demo_1\%PROG%.psopts" -results-dir "%WSP%\R_BF_%BUILD_NUMBER%"   ||  EXIT /B 200
 
 %ps_helper_access% -create-project %PARENT_PROJECT_ON_ACCESS%    ||  EXIT /B 200
 
